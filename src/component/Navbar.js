1e493614c4c9edbcdb9c8fc02d0377489613798a
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2026-01-05 12:54:13 +0000

    chnage in the frontend

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = ({ showAlert }) => {
     useEffect(() => {
         const fetchUserData = async () => {
             try {
-                const response = await fetch("http://localhost:5000/api/auth/getUser", {
+                const response = await fetch("https://inoteproject.onrender.com/api/auth/getUser", {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
