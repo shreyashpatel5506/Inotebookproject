23c673847a7aaf2b211eaa712e6a99ad81a34eec
Shreyash Patel <160259841+shreyashpatel5506@users.noreply.github.com>
2026-01-05 12:36:33 +0000

    change backend url

--- src/component/login.js
+++ src/component/login.js
@@ -9,7 +9,7 @@ const Login = (props) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch("http://localhost:5000/api/auth/loginUser", {
+            const response = await fetch("https://inoteproject.onrender.com/api/auth/loginUser", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
